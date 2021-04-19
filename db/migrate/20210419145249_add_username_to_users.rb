cb788c81fd6c54850d3242f5895a4d93745904d3
Alexandra Lasky <alexandra.lasky@gmail.com>
2021-04-19 19:03:18 +0200

    fixed database tables and added a user reference to cat

--- db/migrate/20210419145249_add_username_to_users.rb
+++ db/migrate/20210419145249_add_username_to_users.rb
@@ -1,4 +1,4 @@
-class AddUsernameToUser < ActiveRecord::Migration[6.0]
+class AddUsernameToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :username, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_150139) do
-
+ActiveRecord::Schema.define(version: 2021_04_19_170041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,9 +50,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_150139) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "bookings", "cats"
   add_foreign_key "bookings", "users"
-
   add_foreign_key "cats", "users"
 end
