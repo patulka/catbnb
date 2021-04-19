3df8f3116d8cf605b733b8fb2e6a2ccab093201e
Patricie Knopova <p.knopova@gmail.com>
2021-04-19 16:26:23 +0200

    added details to cat model + migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_131512) do
+ActiveRecord::Schema.define(version: 2021_04_19_142453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2021_04_19_131512) do
   create_table "cats", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "breed"
+    t.string "color"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_cats_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,4 +31,5 @@ ActiveRecord::Schema.define(version: 2021_04_19_131512) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "cats", "users"
 end
