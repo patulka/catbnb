aa7cdfb0c10b2a101549e67c8b9741199faf3834
Sarka Svobodna <sarka.svobodna@gmail.com>
2021-04-19 21:30:35 +0200

    added gem faker, seed v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning database"
+Cat.destroy_all
+User.destroy_all
+
+puts "Seeding users..."
+
+10.times do
+  user = User.create(
+    email: "Faker::Internet.email",
+    password: "secret",
+    username: ""
+    )
+end
+
+puts "Users created."
+
+puts "Seeding cats..."
+
+30.times do
+  cat = Cat.create(
+    name: "Faker::Name.first_name",
+    breed: "Faker::Creature::Cat.breed",
+    color: "Faker::Color.color_name",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...",
+    user_id: rand(1..30)
+    )
+end
+
+puts "Cats created."
+
+puts "Seeding bookings..."
+
+date = "Faker::Date.in_date_period"
+
+10.times do
+  booking = Booking.create(
+    cat_id: rand(1..30),
+    user_id: rand(1..10),
+    # Random date in current year
+    date_from: date,
+    date_to: date + 7
+    )
+end
+
+puts "Bookings created."
+puts "Seeding finished."
