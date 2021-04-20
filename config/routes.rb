ad117e5ebe7186a9829e7982e0cd948b6554d3f0
Sarka Svobodna <sarka.svobodna@gmail.com>
2021-04-20 16:35:53 +0200

    - minor changes in styling - link to cats#show added

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :cats, only: [:index]
+  resources :cats, only: [:index, :show]
 end
