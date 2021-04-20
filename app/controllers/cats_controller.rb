1ecac315bceb41d8e409565c0b0bf1601dd7908b
Abhilasha Dasari <abhilasha.dasari@gmail.com>
2021-04-20 12:17:40 +0200

    Cat-show update after review

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,11 +1,5 @@
 class CatsController < ApplicationController
-  before_action :find_cat, only: [:show]
-
   def show
     @cat = Cat.find(params[:id])
-  end
-
-  def cat_params
-    params.require(:cat).permit(:name, :breed, :photo, :description, :color)
   end
 end
