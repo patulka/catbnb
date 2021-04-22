e5b06ffea2e2360a65234ca0045fb24d41223eb9
Abhilasha Dasari <abhilasha.dasari@gmail.com>
2021-04-22 11:21:14 +0200

    paws map indent.

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -16,14 +16,18 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+
     const markers = JSON.parse(mapElement.dataset.markers);
+
     markers.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup)
         .addTo(map);
     });
+
     fitMapToMarkers(map, markers);
   }
 };
